553e65a991717d780cdd024cce8898a49cda3581
Simon Willison <simonw@eventbrite.com>
2015-07-12 23:54:13 -0500

    Spiced up the admin a bit

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from .models import Entry, Tag, Quotation, Blogmark
 
-admin.site.register(Entry, raw_id_fields=('tags',))
+class BaseAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created'
+    raw_id_fields = ('tags',)
+    list_display = ('__unicode__', 'slug', 'created', 'tag_summary')
+    
+@admin.register(Entry)
+class EntryAdmin(BaseAdmin):
+    search_fields = ('tags__tag', 'title', 'body')
+
+@admin.register(Quotation)
+class QuotationAdmin(BaseAdmin):
+    search_fields = ('tags__tag', 'quotation')
+
+@admin.register(Blogmark)
+class BlogmarkAdmin(BaseAdmin):
+    search_fields = ('tags__tag', 'commentary')
+
+
 admin.site.register(Tag)
-admin.site.register(Quotation)
-admin.site.register(Blogmark)
