940d3abbc109a675bdb3b7a7ec4d8d2abb0ecb19
Simon Willison <simonw@eventbrite.com>
2017-09-24 13:23:22 -0700

    Fixed STATICFILES_DIRS

--- config/settings.py
+++ config/settings.py
@@ -113,7 +113,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../static/'),
+    os.path.join(BASE_DIR, 'static/'),
 )
 
 # Simplified static file serving.
