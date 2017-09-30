338e8a5faaeceff5a0f9e64eed2e8659cda47497
Simon Willison <simonw@eventbrite.com>
2017-09-30 16:53:30 -0700

    ./manage.py validate_entries_xml for validating XML
    If any of my entries have invalid XML they will break the site.
    
    This command spots any problems.

---
+++ blog/management/commands/validate_entries_xml.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from xml.etree import ElementTree
+from blog.models import Entry
+
+
+class Command(BaseCommand):
+    help = "Spits out list of entries with invalid XML"
+
+    def handle(self, *args, **kwargs):
+        for entry in Entry.objects.all():
+            try:
+                ElementTree.fromstring(
+                    '<entry>%s</entry>' % entry.body.encode('utf8')
+                )
+            except Exception, e:
+                print e
+                print entry.title
+                print 'https://simonwillison.com/admin/blog/entry/%d/' % entry.pk
+                print
