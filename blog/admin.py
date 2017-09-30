7fe14e3ee7cf0094de05c13d50bcfd9b6cdcf198
Simon Willison <simonw@eventbrite.com>
2017-09-30 16:54:53 -0700

    Fixed lint errors

--- blog/admin.py
+++ blog/admin.py
@@ -7,18 +7,22 @@ from .models import (
     Comment
 )
 
+
 class BaseAdmin(admin.ModelAdmin):
     date_hierarchy = 'created'
     raw_id_fields = ('tags',)
     list_display = ('__unicode__', 'slug', 'created', 'tag_summary')
-    
+
+
 @admin.register(Entry)
 class EntryAdmin(BaseAdmin):
     search_fields = ('tags__tag', 'title', 'body')
 
+
 @admin.register(Quotation)
 class QuotationAdmin(BaseAdmin):
     search_fields = ('tags__tag', 'quotation')
+
 
 @admin.register(Blogmark)
 class BlogmarkAdmin(BaseAdmin):
@@ -28,13 +32,12 @@ class BlogmarkAdmin(BaseAdmin):
 admin.site.register(Tag)
 
 admin.site.register(Comment,
-    list_filter = (
+    list_filter=(
         'created', 'visible_on_site', 'spam_status', 'content_type'
     ),
-    search_fields = ('body', 'name', 'url', 'email', 'openid'),
-    list_display = ('name', 'admin_summary', 'on_link', 'created',
+    search_fields=('body', 'name', 'url', 'email', 'openid'),
+    list_display=('name', 'admin_summary', 'on_link', 'created',
         'ip_link', 'visible_on_site', 'spam_status_options'),
-    list_display_links = ('name', 'admin_summary'),
-    date_hierarchy = 'created'
+    list_display_links=('name', 'admin_summary'),
+    date_hierarchy='created'
 )
-
