1262af97ef30500578fcc351c5d9628647cfbc81
Simon Willison <simonw@eventbrite.com>
2017-09-30 15:33:03 -0700

    Fixed a bunch of lint errors

--- blog/templatetags/entry_tags.py
+++ blog/templatetags/entry_tags.py
@@ -1,26 +1,25 @@
 from django import template
 from django.utils.timezone import utc
-register = template.Library()
-
-try:
-    from xml.etree import ElementTree as ET
-except ImportError:
-    from elementtree import ElementTree as ET
-
+from xml.etree import ElementTree
 import re
+import datetime
+
+register = template.Library()
 entry_stripper = re.compile('^<entry>(.*?)</entry>$', re.DOTALL)
 
+
 def _back_to_xhtml(et):
-    m = entry_stripper.match(ET.tostring(et, 'utf-8'))
+    m = entry_stripper.match(ElementTree.tostring(et, 'utf-8'))
     if m:
         return m.group(1)
     else:
         return '' # If we end up with <entry />
 
+
 @register.filter
 def resize_images_to_fit_width(value, arg):
     max_width = int(arg)
-    et = ET.fromstring('<entry>%s</entry>' % value.encode('utf8'))
+    et = ElementTree.fromstring('<entry>%s</entry>' % value.encode('utf8'))
     for img in et.findall('.//img'):
         width = int(img.get('width', 0))
         height = int(img.get('height', 0))
@@ -31,28 +30,33 @@ def resize_images_to_fit_width(value, arg):
     return _back_to_xhtml(et)
 
 xhtml_endtag_fragment = re.compile('\s*/>')
+
+
 @register.filter
 def xhtml2html(xhtml):
     # &apos; is valid in XML/XHTML but not in regular HTML
     s = xhtml.replace('&apos;', '&#39;')
     return xhtml_endtag_fragment.sub('>', s)
 
+
 @register.filter
 def remove_quora_paragraph(xhtml):
-    et = ET.fromstring(('<entry>%s</entry>' % xhtml).encode('utf8'))
+    et = ElementTree.fromstring(('<entry>%s</entry>' % xhtml).encode('utf8'))
     p = et.find('p')
-    if ET.tostring(p, 'utf-8').startswith('<p><em>My answer to'):
+    if ElementTree.tostring(p, 'utf-8').startswith('<p><em>My answer to'):
         et.remove(p)
     return _back_to_xhtml(et)
 
+
 @register.filter
 def first_paragraph(xhtml):
-    et = ET.fromstring(('<entry>%s</entry>' % xhtml).encode('utf8'))
+    et = ElementTree.fromstring(('<entry>%s</entry>' % xhtml).encode('utf8'))
     p = et.find('p')
     if p is not None:
-        return ET.tostring(p, 'utf-8')
+        return ElementTree.tostring(p, 'utf-8')
     else:
         return '<p>%s</p>' % xhtml
+
 
 @register.filter
 def openid_to_url(openid):
@@ -62,30 +66,34 @@ def openid_to_url(openid):
     else:
         return openid
 
+
 @register.filter
 def ends_with_punctuation(value):
-    "Does this end in punctuation? Use to decide if more is needed."
+    """Does this end in punctuation? Use to decide if more is needed."""
     last_char = value.strip()[-1]
     return last_char in '?.!'
 
+
 @register.filter
 def strip_p_ids(xhtml):
-    et = ET.fromstring('<entry>%s</entry>' % xhtml)
+    et = ElementTree.fromstring('<entry>%s</entry>' % xhtml)
     for p in et.findall('.//p'):
         if 'id' in p.attrib:
             del p.attrib['id']
     return _back_to_xhtml(et)
 
+
 @register.filter
 def break_up_long_words(xhtml, length):
-    "Breaks up words that are longer than the argument."
+    """Breaks up words that are longer than the argument."""
     length = int(length)
-    et = ET.fromstring('<entry>%s</entry>' % xhtml)
+    et = ElementTree.fromstring('<entry>%s</entry>' % xhtml)
     do_break_long_words(et, length)
     return _back_to_xhtml(et)
 
+
 def do_break_long_words(et, length):
-    "Pass an ElementTree instance; breaks up long words in it"
+    """Pass an ElementTree instance; breaks up long words in it"""
     if et.text:
         et.text = do_break_long_words_string(et.text, length)
     for child in et.getchildren():
@@ -93,7 +101,10 @@ def do_break_long_words(et, length):
     if et.tail:
         et.tail = do_break_long_words_string(et.tail, length)
 
+
 whitespace_re = re.compile('(\s+)')
+
+
 def do_break_long_words_string(s, length):
     bits = whitespace_re.split(s)
     for i, bit in enumerate(bits):
@@ -107,15 +118,17 @@ def do_break_long_words_string(s, length):
             bits[i] = s
     return ''.join(bits)
 
+
 @register.filter
 def typography(xhtml):
     return xhtml
     if not xhtml:
         return xhtml
     "Handles curly quotes and em dashes. Must be fed valid XHTML!"
-    et = ET.fromstring(u'<entry>%s</entry>' % xhtml.encode('utf8'))
+    et = ElementTree.fromstring(u'<entry>%s</entry>' % xhtml.encode('utf8'))
     do_typography(et)
     return _back_to_xhtml(et)
+
 
 def do_typography(et):
     # Designed to be called recursively on ElementTree objects
@@ -134,6 +147,7 @@ RIGHT_SINGLE_QUOTATION_MARK = u'\u2019'
 QUOTATION_PAIR = (LEFT_DOUBLE_QUOTATION_MARK, RIGHT_DOUBLE_QUOTATION_MARK)
 EM_DASH = u'\u2014'
 
+
 def quote_alternator():
     i = 0
     while True:
@@ -141,6 +155,8 @@ def quote_alternator():
         i += 1
 
 double_re = re.compile('"')
+
+
 def do_typography_string(s):
     if not isinstance(s, unicode):
         s = s.decode('utf-8')
@@ -154,10 +170,11 @@ def do_typography_string(s):
     s = s.replace(' - ', u'\u2014')
     return s
 
-from django.utils.timesince import timesince # Reuse that logic
-import datetime
+
 NUMBERS = 'zero one two three four five six seven eight nine'.split()
 number_re = re.compile('\d+')
+
+
 def num_to_string(s):
     try:
         return NUMBERS[int(s)]
@@ -172,9 +189,10 @@ chunks = (
     (60, 'minute')
 )
 
+
 @register.filter
 def text_ago(d):
-    "Returns 'One day' or 'Three minutes' etc - similar to time_since"
+    """Returns 'One day' or 'Three minutes' etc - similar to time_since"""
     delta = datetime.datetime.utcnow().replace(tzinfo=utc) - d
     since_seconds = (24 * 60 * 60 * delta.days) + delta.seconds
     for i, (seconds, name) in enumerate(chunks):
@@ -189,6 +207,7 @@ def text_ago(d):
         text = 'moments'
     return '%s ago' % text
 
+
 @register.inclusion_tag('includes/entry_footer.html', takes_context=True)
 def entry_footer(context, entry):
     context.update({
@@ -196,6 +215,7 @@ def entry_footer(context, entry):
         'showdate': True
     })
     return context
+
 
 @register.inclusion_tag('includes/entry_footer.html', takes_context=True)
 def entry_footer_no_date(context, entry):
