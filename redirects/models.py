22b4c1e062904be465fe0306c7777bf79b82e725
Simon Willison <simonw@eventbrite.com>
2017-10-01 15:42:45 -0700

    Redirect fields should not allow null

--- redirects/models.py
+++ redirects/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Redirect(models.Model):
-    domain = models.CharField(max_length=128, blank=True, null=True)
-    path = models.CharField(max_length=128, blank=True, null=True)
-    target = models.CharField(max_length=256, blank=True, null=True)
+    domain = models.CharField(max_length=128, blank=True)
+    path = models.CharField(max_length=128, blank=True)
+    target = models.CharField(max_length=256, blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
