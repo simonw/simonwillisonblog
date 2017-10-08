bbe094261031d7fe9647a8fca1ff24bd2ea30801
Simon Willison <simonw@eventbrite.com>
2017-10-08 13:07:22 -0700

    Support for path wildcards in redirect middleware
    I want to be able to redirect all www.simonwillison.net/... paths to the
    correct place on simonwillison.net instead.
    
    I've taught my redirects app to understand wildcards. Now I can create a
    redirect rule that looks like this:
    
        domain = www.simonwillison.net
        path = *
        target = https://simonwillison.net/
    
    And the redirect middleware will know to redirect anything starting with
    www.simonwillison.net to the equivalent URL on https://simonwillison.net

--- redirects/middleware.py
+++ redirects/middleware.py
@@ -4,14 +4,23 @@ from django.http import HttpResponsePermanentRedirect
 
 def redirect_middleware(get_response):
     def middleware(request):
-        try:
-            redirect = Redirect.objects.get(
-                domain=request.get_host(),
-                path=request.path.lstrip('/')
-            )
-            return HttpResponsePermanentRedirect(redirect.target)
-        except Redirect.DoesNotExist:
-            pass
+        path = request.path.lstrip('/')
+        redirects = list(Redirect.objects.filter(
+            domain=request.get_host(),
+            # We redirect on either a path match or a '*'
+            # record existing for this domain
+            path__in=(path, '*')
+        ))
+        # A non-star redirect always takes precedence
+        non_star = [r for r in redirects if r.path != '*']
+        if non_star:
+            return HttpResponsePermanentRedirect(non_star[0].target)
+        # If there's a star redirect, build path and redirect to that
+        star = [r for r in redirects if r.path == '*']
+        if star:
+            new_url = star[0].target + path
+            return HttpResponsePermanentRedirect(new_url)
+        # Default: no redirects, just get on with it:
         return get_response(request)
 
     return middleware
