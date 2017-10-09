e72e45d70367a7a0a18b3462808d438f88c8c338
Simon Willison <simonw@eventbrite.com>
2017-10-08 21:51:32 -0700

    Initial, incomplete attempt at porting to Python 3

--- blog/models.py
+++ blog/models.py
@@ -81,6 +81,7 @@ class BaseModel(models.Model):
     slug = models.SlugField(max_length=64)
     metadata = JSONField(blank=True, default={})
     search_document = SearchVectorField(null=True)
+    private = models.BooleanField(default=False)
 
     def created_unixtimestamp(self):
         return arrow.get(self.created).timestamp
