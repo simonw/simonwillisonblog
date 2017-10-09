f78abfdf652c4a65ba1c3b72ddc39214a58240cf
Simon Willison <simonw@eventbrite.com>
2017-10-08 20:57:44 -0700

    Added very basic initial unit tests
    Just testing the homepage and the entry/blogmark/quotation pages at the moment.

---
+++ blog/factories.py
@@ -0,0 +1,30 @@
+import factory
+import factory.django
+import factory.fuzzy
+from django.utils.timezone import utc
+
+
+class BaseFactory(factory.django.DjangoModelFactory):
+    slug = factory.Sequence(lambda n: 'slug%d' % n)
+    created = factory.Faker('past_datetime', tzinfo=utc)
+
+
+class EntryFactory(BaseFactory):
+    class Meta:
+        model = 'blog.Entry'
+
+    title = factory.Faker('sentence')
+
+
+class BlogmarkFactory(BaseFactory):
+    class Meta:
+        model = 'blog.Blogmark'
+
+    link_url = factory.Faker('uri')
+    link_title = factory.Faker('sentence')
+    commentary = factory.Faker('sentence')
+
+
+class QuotationFactory(BaseFactory):
+    class Meta:
+        model = 'blog.Quotation'
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from .factories import (
+    EntryFactory,
+    BlogmarkFactory,
+    QuotationFactory,
+)
 
-# Create your tests here.
+
+class BlogTests(TestCase):
+    def test_homepage(self):
+        db_entries = [
+            EntryFactory(),
+            EntryFactory(),
+            EntryFactory(),
+        ]
+        BlogmarkFactory()
+        QuotationFactory()
+        response = self.client.get('/')
+        entries = response.context['entries']
+        self.assertEqual(
+            [e.pk for e in entries],
+            [e.pk for e in sorted(
+                db_entries,
+                key=lambda e: e.created, reverse=True
+            )]
+        )
+
+    def test_entry(self):
+        entry = EntryFactory()
+        response = self.client.get(entry.get_absolute_url())
+        self.assertTemplateUsed(response, 'entry.html')
+        self.assertEqual(response.context['entry'].pk, entry.pk)
+
+    def test_blogmark(self):
+        blogmark = BlogmarkFactory()
+        response = self.client.get(blogmark.get_absolute_url())
+        self.assertTemplateUsed(response, 'blogmark.html')
+        self.assertEqual(response.context['blogmark'].pk, blogmark.pk)
+
+    def test_quotation(self):
+        quotation = QuotationFactory()
+        response = self.client.get(quotation.get_absolute_url())
+        self.assertTemplateUsed(response, 'quotation.html')
+        self.assertEqual(response.context['quotation'].pk, quotation.pk)
