679b1736095221da801a4fcc0b9ee9c5df449af5
Simon Willison <simonw@eventbrite.com>
2017-10-14 22:58:13 -0700

    Work around empty REMOTE_ADDR field

--- cloudflareips/middleware.py
+++ cloudflareips/middleware.py
@@ -6,7 +6,7 @@ CF_HEADER = 'HTTP_CF_CONNECTING_IP'
 def cloudflare_ip_middleware(get_response):
     def middleware(request):
         remote_addr = request.META['REMOTE_ADDR']
-        if is_cloudflare(remote_addr) and CF_HEADER in request.META:
+        if remote_addr and is_cloudflare(remote_addr) and CF_HEADER in request.META:
             request.META['REMOTE_ADDR'] = request.META[CF_HEADER]
         return get_response(request)
 
