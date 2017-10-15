4d258790f85fdbeda15ce7332a038a4e795221eb
Simon Willison <simonw@eventbrite.com>
2017-10-14 20:25:29 -0700

    Feedstats (#5)
    * Mechanism for counting feed subscribers based on User-Agent
    
    A number of feed reader services expose subscriber counts in their user-agent
    string, like this:
    
    * NewsGatorOnline/2.0 (http://www.newsgator.com; 47 subscribers)
    * NewsBlur Feed Fetcher - 496 subscribers - ...
    * Feedbin feed-id:379793 - 21 subscribers
    
    The new feedstats app looks out for this pattern of user-agent string and
    stores a row in the new SubscriberCount model every time it spots a new number
    of subscribers. This will allow me to track subscriber growth over time.
    
    * Run migrations automatically when app is deployed
    
    https://devcenter.heroku.com/articles/release-phase#when-does-the-release-command-run

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'blog',
     'redirects',
+    'feedstats',
 )
 
 MIDDLEWARE = (
---
+++ feedstats/utils.py
@@ -0,0 +1,33 @@
+from functools import wraps
+from .models import SubscriberCount
+import datetime
+import re
+
+subscribers_re = re.compile('(\d+) subscribers')
+
+
+def count_subscribers(view_fn):
+    @wraps(view_fn)
+    def inner_fn(request, *args, **kwargs):
+        user_agent = request.META.get('HTTP_USER_AGENT', '')
+        match = subscribers_re.search(user_agent)
+        if match:
+            count = int(match.group(1))
+            today = datetime.date.today()
+            simplified_user_agent = subscribers_re.sub('X subscribers', user_agent)
+            # Do we have this one yet?
+            if not SubscriberCount.objects.filter(
+                path=request.path,
+                count=count,
+                user_agent=simplified_user_agent,
+                created__year=today.year,
+                created__month=today.month,
+                created__day=today.day,
+            ).exists():
+                SubscriberCount.objects.create(
+                    path=request.path,
+                    count=count,
+                    user_agent=simplified_user_agent,
+                )
+        return view_fn(request, *args, **kwargs)
+    return inner_fn
