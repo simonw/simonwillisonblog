simonw/simonwillisonblog

commit 52d083913c3645636cbf866a2a1d63b26e164c36

Python 3 fixes after manual testing
Mainly the following:

* Remove all utf8 encode/decode from my ElementTree code
* Ensure models all have a __str__ representation method
* Replace list.sort(lambda x, y: cmp(x, y)) with key= instead

blog/views.py
@@ -286,7 +286,7 @@ def archive_day(request, year, month, day):
         for ps in context['photoset']])
     if count == 0:
         raise Http404("No photosets/photos/entries/quotes/links for that day")
-    items.sort(key=lambda x, y: cmp(x['obj'].created, y['obj'].created))
+    items.sort(key=lambda x: x['obj'].created)
     context['items'] = items
     photos = Photo.objects.filter(
         created__year=context['date'].year,
@@ -341,7 +341,7 @@ def archive_tag(request, tags):
         ])
     if not items:
         raise Http404
-    items.sort(key=lambda x, y: cmp(y['obj'].created, x['obj'].created))
+    items.sort(key=lambda x: x['obj'].created)
     # Paginate it
     paginator = Paginator(items, 30)
     page_number = request.GET.get('page') or '1'

blog/admin.py
@@ -13,7 +13,7 @@ from .models import (
 class BaseAdmin(admin.ModelAdmin):
     date_hierarchy = 'created'
     raw_id_fields = ('tags',)
-    list_display = ('__unicode__', 'slug', 'created', 'tag_summary')
+    list_display = ('__str__', 'slug', 'created', 'tag_summary')
 
 
 class MyEntryForm(forms.ModelForm):
@@ -22,7 +22,7 @@ class MyEntryForm(forms.ModelForm):
         body = self.cleaned_data['body']
         try:
             ElementTree.fromstring(
-                '<entry>%s</entry>' % body.encode('utf8')
+                '<entry>%s</entry>' % body
             )
         except Exception as e:
             raise forms.ValidationError(str(e))

blog/models.py
@@ -20,7 +20,7 @@ class Tag(models.Model):
         unique=True
     )
 
-    def __unicode__(self):
+    def __str__(self):
         return self.tag
 
     def get_absolute_url(self):
@@ -130,7 +130,7 @@ class Entry(BaseModel):
             'B': ' '.join(self.tags.values_list('tag', flat=True)),
         }
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
 
 
@@ -152,7 +152,7 @@ class Quotation(BaseModel):
             'C': self.source,
         }
 
-    def __unicode__(self):
+    def __str__(self):
         return self.quotation
 
 
@@ -172,7 +172,7 @@ class Blogmark(BaseModel):
             'C': self.commentary + ' ' + self.link_domain() + ' ' + (self.via_title or ''),
         }
 
-    def __unicode__(self):
+    def __str__(self):
         return self.link_title
 
     def link_domain(self):
@@ -195,7 +195,7 @@ class Photo(models.Model):
     latitude = models.CharField(max_length=32, blank=True, null=True)
     created = models.DateTimeField()
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
 
     def photopage(self):
@@ -280,7 +280,7 @@ class Comment(models.Model):
     def edit_url(self):
         return "/admin/blog/comment/%d/" % self.id
 
-    def __unicode__(self):
+    def __str__(self):
         return '%s on "%s"' % (self.name, self.item)
 
     def admin_summary(self):

blog/templatetags/blog_calendar.py
@@ -94,7 +94,7 @@ def calendar_context(date):
             day['populated'] = True
     # Now that we've gathered the data we can render the calendar
     days = list(day_things.values())
-    days.sort(lambda x, y: cmp(x['day'], y['day']))
+    days.sort(key=lambda x: x['day'])
     # But first, swoop through and add a description to every day
     for day in days:
         day['score'] = score_for_day(day)

blog/templatetags/entry_tags.py
@@ -5,13 +5,13 @@ import re
 import datetime
 
 register = template.Library()
-entry_stripper = re.compile(b'^<entry>(.*?)</entry>$', re.DOTALL)
+entry_stripper = re.compile('^<entry>(.*?)</entry>$', re.DOTALL)
 
 
 def _back_to_xhtml(et):
-    m = entry_stripper.match(ElementTree.tostring(et, 'utf-8'))
+    m = entry_stripper.match(ElementTree.tostring(et, 'unicode'))
     if m:
-        return m.group(1).decode('utf-8')
+        return m.group(1)
     else:
         return '' # If we end up with <entry />
 
@@ -19,7 +19,7 @@ def _back_to_xhtml(et):
 @register.filter
 def resize_images_to_fit_width(value, arg):
     max_width = int(arg)
-    et = ElementTree.fromstring('<entry>%s</entry>' % value.encode('utf8'))
+    et = ElementTree.fromstring('<entry>%s</entry>' % value)
     for img in et.findall('.//img'):
         width = int(img.get('width', 0))
         height = int(img.get('height', 0))
@@ -41,23 +41,23 @@ def xhtml2html(xhtml):
 
 @register.filter
 def remove_quora_paragraph(xhtml):
-    et = ElementTree.fromstring(('<entry>%s</entry>' % xhtml).encode('utf8'))
+    et = ElementTree.fromstring('<entry>%s</entry>' % xhtml)
     p = et.find('p')
     if p is None:
         return _back_to_xhtml(et)
-    if ElementTree.tostring(p, 'utf-8').startswith('<p><em>My answer to'):
+    if ElementTree.tostring(p, 'unicode').startswith('<p><em>My answer to'):
         et.remove(p)
     return _back_to_xhtml(et)
 
 
 @register.filter
 def first_paragraph(xhtml):
-    et = ElementTree.fromstring(('<entry>%s</entry>' % xhtml).encode('utf8'))
+    et = ElementTree.fromstring(('<entry>%s</entry>' % xhtml))
     p = et.find('p')
     if p is not None:
-        return ElementTree.tostring(p, 'utf-8').decode('utf-8')
+        return ElementTree.tostring(p, 'unicode')
     else:
-        return ('<p>%s</p>' % xhtml).decode('utf-8')
+        return ('<p>%s</p>' % xhtml)
 
 
 @register.filter
@@ -78,7 +78,7 @@ def ends_with_punctuation(value):
 
 @register.filter
 def strip_p_ids(xhtml):
-    et = ElementTree.fromstring('<entry>%s</entry>' % xhtml.encode('utf-8'))
+    et = ElementTree.fromstring('<entry>%s</entry>' % xhtml)
     for p in et.findall('.//p'):
         if 'id' in p.attrib:
             del p.attrib['id']
@@ -89,7 +89,7 @@ def strip_p_ids(xhtml):
 def break_up_long_words(xhtml, length):
     """Breaks up words that are longer than the argument."""
     length = int(length)
-    et = ElementTree.fromstring('<entry>%s</entry>' % xhtml.encode('utf-8'))
+    et = ElementTree.fromstring('<entry>%s</entry>' % xhtml)
     do_break_long_words(et, length)
     return _back_to_xhtml(et)
 
@@ -123,11 +123,10 @@ def do_break_long_words_string(s, length):
 
 @register.filter
 def typography(xhtml):
-    return xhtml
     if not xhtml:
         return xhtml
     "Handles curly quotes and em dashes. Must be fed valid XHTML!"
-    et = ElementTree.fromstring('<entry>%s</entry>' % xhtml.encode('utf8'))
+    et = ElementTree.fromstring('<entry>%s</entry>' % xhtml)
     do_typography(et)
     return _back_to_xhtml(et)
 
@@ -160,8 +159,6 @@ double_re = re.compile('"')
 
 
 def do_typography_string(s):
-    if not isinstance(s, str):
-        s = s.decode('utf-8')
     # Do single quotes
     s = s.replace("'", RIGHT_SINGLE_QUOTATION_MARK)
     # Now do double quotes, but only if an even number of them

commit c5f8b8923851b3e80c93e907f78351f50b894cf3

Fixed bug where some things were in reverse order
Introduced in 52d083913c3645

blog/views.py
@@ -121,7 +121,7 @@ def index(request):
             if q.created.date() == day
         ]
         items = links + quotes
-        items.sort(key=lambda x: x['date'])
+        items.sort(key=lambda x: x['date'], reverse=True)
         days.append({
             'date': day,
             'items': items,
@@ -286,7 +286,7 @@ def archive_day(request, year, month, day):
         for ps in context['photoset']])
     if count == 0:
         raise Http404("No photosets/photos/entries/quotes/links for that day")
-    items.sort(key=lambda x: x['obj'].created)
+    items.sort(key=lambda x: x['obj'].created, reverse=True)
     context['items'] = items
     photos = Photo.objects.filter(
         created__year=context['date'].year,
@@ -341,7 +341,7 @@ def archive_tag(request, tags):
         ])
     if not items:
         raise Http404
-    items.sort(key=lambda x: x['obj'].created)
+    items.sort(key=lambda x: x['obj'].created, reverse=True)
     # Paginate it
     paginator = Paginator(items, 30)
     page_number = request.GET.get('page') or '1'