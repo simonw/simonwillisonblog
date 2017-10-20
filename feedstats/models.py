c3baa498e88ee846e4b86b8ce3b63979ac484490
Simon Willison <simonw@eventbrite.com>
2017-10-20 11:56:34 -0700

    Fix #9 - longer user agent DB column

--- feedstats/models.py
+++ feedstats/models.py
@@ -7,7 +7,7 @@ class SubscriberCount(models.Model):
     path = models.CharField(max_length=128)
     count = models.IntegerField()
     created = models.DateTimeField(auto_now_add=True)
-    user_agent = models.CharField(max_length=128, db_index=True)
+    user_agent = models.CharField(max_length=256, db_index=True)
 
     class Meta:
         index_together = [
