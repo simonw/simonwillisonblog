f970fbfc5cf565e08970b393411b8e8df4dc9b61
Simon Willison <swillison@gmail.com>
2018-11-04 22:10:03 -0800

    Show 6 entries on homepage

--- blog/views.py
+++ blog/views.py
@@ -144,7 +144,7 @@ def index(request):
 
     response = render(request, 'homepage.html', {
         'days': days,
-        'entries': Entry.objects.prefetch_related('tags')[0:4],
+        'entries': Entry.objects.prefetch_related('tags')[0:6],
         'current_tags': find_current_tags(5),
     })
     response['Cache-Control'] = 's-maxage=200'
