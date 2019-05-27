1ad63c954bf5c24b7bcb23820162e8b868107969
Simon Willison <swillison@gmail.com>
2019-05-26 17:45:09 -0700

    Applied black prior to bug fix

--- blog/signals.py
+++ blog/signals.py
@@ -1,6 +1,6 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save, m2m_changed
-from django.db.models import Value
+from django.db.models import Value, TextField
 from django.contrib.postgres.search import SearchVector
 from django.db import transaction
 from blog.models import BaseModel, Tag
@@ -12,17 +12,17 @@ from functools import reduce
 def on_save(sender, **kwargs):
     if not issubclass(sender, BaseModel):
         return
-    transaction.on_commit(make_updater(kwargs['instance']))
+    transaction.on_commit(make_updater(kwargs["instance"]))
 
 
 @receiver(m2m_changed)
 def on_m2m_changed(sender, **kwargs):
-    instance = kwargs['instance']
-    model = kwargs['model']
+    instance = kwargs["instance"]
+    model = kwargs["model"]
     if model is Tag:
         transaction.on_commit(make_updater(instance))
     elif isinstance(instance, Tag):
-        for obj in model.objects.filter(pk__in=kwargs['pk_set']):
+        for obj in model.objects.filter(pk__in=kwargs["pk_set"]):
             transaction.on_commit(make_updater(obj))
 
 
@@ -33,10 +33,9 @@ def make_updater(instance):
     def on_commit():
         search_vectors = []
         for weight, text in list(components.items()):
-            search_vectors.append(
-                SearchVector(Value(text), weight=weight)
-            )
+            search_vectors.append(SearchVector(Value(text), weight=weight))
         instance.__class__.objects.filter(pk=pk).update(
             search_document=reduce(operator.add, search_vectors)
         )
+
     return on_commit
