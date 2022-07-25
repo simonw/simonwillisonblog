bf61228ddb700970e8be38912547a381a13d529b
Simon Willison <swillison@gmail.com>
2022-07-25 16:27:03 -0700

    Disable XFrameOptionsMiddleware
    I want to try my site at different widths using https://github.com/simonw/viewport-preview

--- config/settings.py
+++ config/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = (
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+#    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 )
 if DEBUG:
     MIDDLEWARE = ("debug_toolbar.middleware.DebugToolbarMiddleware",) + MIDDLEWARE
