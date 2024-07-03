1e6c5406d1435b3fa47f2755927a7bcd7e51c617
Simon Willison <swillison@gmail.com>
2024-07-03 15:25:02 -0700

    Add tags autocomplete to search page (#458)

---
+++ blog/tag_views.py
@@ -0,0 +1,18 @@
+from django.http import JsonResponse
+from django.db.models.functions import Length
+from .models import Tag
+
+
+def tags_autocomplete(request):
+    query = request.GET.get("q", "")
+    if query:
+        tags = (
+            Tag.objects.filter(tag__icontains=query)
+            .annotate(tag_length=Length("tag"))
+            .order_by("tag_length")[:5]
+        )
+    else:
+        tags = Tag.objects.none()
+    return JsonResponse(
+        {"tags": [{"tag": tag.tag, "count": tag.total_count()} for tag in tags]}
+    )
