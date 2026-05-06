e3054cf37e70dbb081a120ea4f3a1094d1c6d669
Simon Willison <swillison@gmail.com>
2026-05-05 22:54:53 -0700

    s3_web_manager_django-0.1.2
    Has responsive viewport.

--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 DJANGO_DEBUG=1 \
-  uv run --with-requirements requirements.txt \
+  uv run \
+    --with-requirements <(grep -v s3_web_manager_django requirements.txt) \
+    --with-editable ~/dev/s3-web-manager-django \
     python ./manage.py runserver 0.0.0.0:8033
